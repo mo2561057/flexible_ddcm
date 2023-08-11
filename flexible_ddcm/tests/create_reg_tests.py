2891b461e3847cab2a5ef154d316e2f6da329b1a
mo2561057 <moritz.mendel12@gmail.com>
2023-08-11 13:44:18 +0200

    Add regression tests

---
+++ flexible_ddcm/tests/create_reg_tests.py
@@ -0,0 +1,58 @@
+"""Entry point script"""
+import pickle as pkl
+
+import numpy as np
+import pandas as pd
+import yaml
+
+from flexible_ddcm.estimation_utils import process_simulation_dict
+from flexible_ddcm.example.base.input_functions import (
+    map_transition_to_state_choice_entries_nonstandard,
+)
+from flexible_ddcm.example.base.input_functions import reward_function_nonstandard
+from flexible_ddcm.example.base.input_functions import transition_function_nonstandard
+from flexible_ddcm.simulate import get_simulate_func
+from flexible_ddcm.solve import solve
+
+from flexible_ddcm.state_space import create_state_space
+
+
+def create_reg_vault(
+        params,
+        model_options,
+        external_probabilities,
+        seed):
+    
+    np.random.seed(seed)
+    simulate = get_simulate_func(
+    model_options,
+    transition_function_nonstandard,
+    reward_function_nonstandard,
+    external_probabilities,
+    map_transition_to_state_choice_entries_nonstandard,
+    )
+
+    simulate_dict = simulate(params)
+
+    vault = (simulate_dict, params, model_options, external_probabilities, seed)
+
+    with open("tests/resources/reg_vault.pkl","wb") as writer:
+        pkl.dump(vault, writer)
+    
+
+
+if __name__=="main":
+    params = pd.read_csv(
+        "flexible_ddcm/tests/resources/params.csv").set_index(["category", "name"])[["value"]]
+    model_options = yaml.safe_load(open("flexible_ddcm/tests/resources/specification.csv"))
+    external_probabilities = pd.read_csv(
+        "flexible_ddcm/tests/resources/external_probabilities.csv"
+    ).drop(columns=["Unnamed: 0"])
+
+    seed_base = 897 
+    create_reg_vault(
+        params,
+        model_options,
+        external_probabilities,
+        seed_base
+        )
--- flexible_ddcm/tests/test_regression.py
+++ flexible_ddcm/tests/test_regression.py
@@ -1,40 +1,41 @@
 """Entry point script"""
+import pickle as pkl
+
 import numpy as np
 import pandas as pd
 import yaml
 
 from flexible_ddcm.estimation_utils import process_simulation_dict
-from flexible_ddcm.example.types.input_functions import (
+from flexible_ddcm.example.base.input_functions import (
     map_transition_to_state_choice_entries_nonstandard,
 )
-from flexible_ddcm.example.types.input_functions import reward_function_nonstandard
-from flexible_ddcm.example.types.input_functions import transition_function_nonstandard
+from flexible_ddcm.example.base.input_functions import reward_function_nonstandard
+from flexible_ddcm.example.base.input_functions import transition_function_nonstandard
 from flexible_ddcm.simulate import get_simulate_func
 from flexible_ddcm.solve import solve
 
 from flexible_ddcm.state_space import create_state_space
 
 
-def create_reg_vault(
-        params,
-        model_options,
-        external_probabilities,
-        seed):
-    
+def test_regression_base():
+    simulate_dict, params, model_options, external_probabilities, seed = pd.read_pickle(
+        "tests/resources/reg_vault.pkl")
     np.random.seed(seed)
     simulate = get_simulate_func(
-    model_options,
-    transition_function_nonstandard,
-    reward_function_nonstandard,
-    external_probabilities,
-    map_transition_to_state_choice_entries_nonstandard,
-    )
+        model_options,
+        transition_function_nonstandard,
+        reward_function_nonstandard,
+        external_probabilities,
+        map_transition_to_state_choice_entries_nonstandard,
+        )
 
-    simulate_dict = simulate(params)
+    simulate_dict_actual = simulate(params)
 
-    vault = (simulate_dict, params, model_options, external_probabilities)
-
-    with open()
+    # Compare vault top new thing:
     
+    key_ = max(simulate_dict.keys())
 
-def test_regression():
+    assert simulate_dict[
+        key_].age.mean() == simulate_dict_actual[key_].age.mean()
+
+
