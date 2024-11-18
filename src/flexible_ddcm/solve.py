a167fb1f206f2f0908fa91a122d4989cb22c2936
mo2561057 <moritz.mendel12@gmail.com>
2024-11-18 14:17:07 +0200

    Change dtype of rewards

--- src/flexible_ddcm/solve.py
+++ src/flexible_ddcm/solve.py
@@ -140,7 +140,7 @@ def get_continuation_value_for_transitions(
         state_space.state_and_choice_to_state_choice[choice][transitions.index.values].astype(int)
     ]
 
-    return transitions.values * (rewards.values + continuation_values * discount)
+    return transitions.values * (rewards + continuation_values * discount)
 
 
 def _map_continuation_to_transition(
